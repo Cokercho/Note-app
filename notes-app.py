dbc1a989e11abd490f3c3de43db2a70b26cc45ce
Coker <viktordimov687@gmail.com>
2026-03-29 22:52:12 +0300

    Add files via upload

---
+++ notes-app.py
@@ -0,0 +1,53 @@
+import json
+from datetime import datetime
+
+try:
+    with open("notes.json","r") as file:
+        notes=json.load(file)
+except:
+   notes=[]
+
+while True:
+    print("Welcome to the note app! pick an option")
+    print("1: Add a new note")
+    print("2: Search note by day")
+    print("3: view all notes")
+
+
+    try:
+        choice=int(input("Enter a number: "))
+    except ValueError:
+        print("Invalid choice")
+        continue
+
+
+    if choice==1:
+            user_note=input("Write a note: ")
+            now=datetime.now().strftime("%d/%m/%Y")
+
+            notes.append({
+            "Note": user_note,
+            "Date": now
+            })
+            with open("notes.json","w") as file:
+                json.dump(notes, file)
+            print("Note saved!")
+
+
+    elif choice==2:
+        search_date=input("Enter a date (d/m/y): ")
+        for note in notes:
+            if note['Date'] == search_date:
+                print("Note found!")
+                print(note["Note"])
+            else:
+                print("Note not found")
+
+    elif choice==3:
+        for note in notes:
+            print(note["Note"], "-", note["Date"])
+
+
+    else:
+        print("Not an option")
+        continue
